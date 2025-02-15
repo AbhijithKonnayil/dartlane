cccf367b1e1779ea9a01458457855bef3ea3dce2
Abhijith K <iamabhijith.k@gmail.com>
2025-02-15 23:31:14 +0530

    init project

---
+++ lib/dartlane.dart
@@ -0,0 +1,10 @@
+/// dartlane, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate dartlane
+/// dart pub global activate dartlane
+///
+/// # see usage
+/// dartlane --help
+/// ```
+library;
