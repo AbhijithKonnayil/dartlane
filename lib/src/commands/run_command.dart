fb3884f8273de4751c581fa54a6b4e47b06c3c12
Abhijith K <iamabhijith.k@gmail.com>
2025-02-19 03:35:12 +0530

    feat: implement custom lane execution in a separate isolate

--- lib/src/commands/run_command.dart
+++ lib/src/commands/run_command.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:args/command_runner.dart';
 import 'package:dartlane/src/core/execute_cusomlane.dart';
+import 'package:dartlane/src/core/files.dart';
 import 'package:io/io.dart';
 
 import '../core/logger.dart';
@@ -25,5 +27,27 @@ class RunCommand extends Command<int> {
     final projectPath = Directory.current.path;
     await executeCustomLane(projectPath: projectPath, laneName: laneName);
     return ExitCode.success.code;
+  }
+
+  Future<void> executeCustomLane({
+    required String projectPath,
+    required String laneName,
+  }) async {
+    final lanesFile = findDartlaneLanesFile(projectPath);
+    if (lanesFile == null) {
+      _logger.err('lanes.dart not found in $projectPath/dartlane');
+      return;
+    }
+
+    // Spawn an isolate to run the custom lane
+    final receivePort = ReceivePort();
+    await Isolate.spawn(runCustomLaneInIsolate, receivePort.sendPort);
+
+    // Send the path of the lanes file to the isolate
+    final sendPort = await receivePort.first as SendPort;
+    sendPort.send({
+      'lanesFilePath': lanesFile.path,
+      'laneName': laneName,
+    });
   }
 }
