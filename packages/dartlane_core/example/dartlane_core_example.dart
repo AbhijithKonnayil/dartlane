9f672aa7aab37081730853ab7607d8aa385adcf5
Abhijith K <iamabhijith.k@gmail.com>
2025-04-23 06:54:49 +0530

    feat: refactor by moving the Lane Interface , Logger and status enum to dartlane_core

--- packages/dartlane_core/example/dartlane_core_example.dart
+++ packages/dartlane_core/example/dartlane_core_example.dart
@@ -1,6 +1,1 @@
-import 'package:dartlane_core/dartlane_core.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+void main() {}
--- packages/dartlane_core/lib/dartlane_core.dart
+++ packages/dartlane_core/lib/dartlane_core.dart
@@ -1,8 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-export 'src/dartlane_core_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export './src/core/lane.dart';
+export './src/core/logger.dart';
+export './src/core/enums.dart';
